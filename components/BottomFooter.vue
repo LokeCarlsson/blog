ba73248ef66d49eaedc077287e8424fa1785eb9f
LokeCarlsson <carlssonloke@gmail.com>
2018-12-29 12:44:31 +0100

    Fix language

--- components/BottomFooter.vue
+++ components/BottomFooter.vue
@@ -1,12 +1,12 @@
 <template>
   <footer class="bottom-footer">
     <div class="util__container">
-      <nuxt-link class="bottom-footer__link" to="/en/sitemap">Sitemap</nuxt-link>
+      <nuxt-link class="bottom-footer__link" :to="'/' + $store.state.language + '/sitemap'">Sitemap</nuxt-link>
     </div>
   </footer>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
 .bottom-footer {
   background: #e3f2ed;
   padding: 40px 0 120px 0;
